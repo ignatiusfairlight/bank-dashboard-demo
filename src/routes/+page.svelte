4cd4375d7f5294dedf4e940b8d8fcfe363c8dcc8
ignatiusfairlight <lawaierang@gmail.com>
2025-07-23 16:10:05 +0800

    Added dropdown menu to My Accounts' cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
+    import EllipsisIcon from "@lucide/svelte/icons/ellipsis";
     import * as Card from "$lib/components/ui/card/index.js";
+    import * as DropdownMenu from "$lib/components/ui/dropdown-menu/index.js";
+    import { Button } from "$lib/components/ui/button/index.js";
+	import DropdownMenuContent from "$lib/components/ui/dropdown-menu/dropdown-menu-content.svelte";
 </script>
 
 <div class="flex flex-col gap-10">
@@ -7,35 +11,79 @@
     <div class="flex flex-col gap-5">
         <Card.Root>
             <Card.Header class="flex flex-col justify-center items-center">
-                <Card.Title>
-                    <h2 class="text-3xl md:text-4xl">RM 44,500.00</h2>
+                <Card.Title class="text-3xl md:text-4xl">
+                    RM 44,500.00
                 </Card.Title>
                 <Card.Description>
-                    <h3>Total Balance</h3>
+                    Total Balance
                 </Card.Description>
             </Card.Header>
         </Card.Root>
         <Card.Root class="bg-red-400">
-            <Card.Header class="flex flex-col justify-center items-start">
-                <Card.Title>
-                    <h2 class="text-2xl">Savings Account-Alpha</h2>
+            <Card.Header>
+                <Card.Title class="text-2xl">
+                    Savings Account-Alpha
                 </Card.Title>
                 <Card.Description>
-                    <h3>7483 9162 5084</h3>
+                    7483 9162 5084
                 </Card.Description>
+                <Card.Action>
+                    <DropdownMenu.Root>
+                        <DropdownMenu.Trigger>
+                            {#snippet child({ props })}
+                                <Button
+                                    {...props}
+                                    variant="link"
+                                    size="icon"
+                                    class="relative size-8 p-0"
+                                >
+                                    <span class="sr-only">Open Menu</span>
+                                    <EllipsisIcon />
+                                </Button>
+                            {/snippet}
+                        </DropdownMenu.Trigger>
+                        <DropdownMenu.Content>
+                            <DropdownMenu.Item>Item 1</DropdownMenu.Item>
+                            <DropdownMenu.Item>Item 2</DropdownMenu.Item>
+                            <DropdownMenu.Item>Item 3</DropdownMenu.Item>
+                        </DropdownMenu.Content>
+                    </DropdownMenu.Root>
+                </Card.Action>
             </Card.Header>
             <Card.Content>
                 <p>RM20,000.00</p>
             </Card.Content>
         </Card.Root>
         <Card.Root class="bg-amber-400">
-            <Card.Header class="flex flex-col justify-center items-start">
-                <Card.Title>
-                    <h2 class="text-2xl">Current Account-i</h2>
+            <Card.Header>
+                <Card.Title class="text-2xl">
+                    Current Account-i
                 </Card.Title>
                 <Card.Description>
-                    <h3>7483 9162 5084</h3>
+                    7483 9162 5084
                 </Card.Description>
+                <Card.Action>
+                    <DropdownMenu.Root>
+                        <DropdownMenu.Trigger>
+                            {#snippet child({ props })}
+                                <Button
+                                    {...props}
+                                    variant="link"
+                                    size="icon"
+                                    class="relative size-8 p-0"
+                                >
+                                    <span class="sr-only">Open Menu</span>
+                                    <EllipsisIcon />
+                                </Button>
+                            {/snippet}
+                        </DropdownMenu.Trigger>
+                        <DropdownMenu.Content>
+                            <DropdownMenu.Item>Item 1</DropdownMenu.Item>
+                            <DropdownMenu.Item>Item 2</DropdownMenu.Item>
+                            <DropdownMenu.Item>Item 3</DropdownMenu.Item>
+                        </DropdownMenu.Content>
+                    </DropdownMenu.Root>
+                </Card.Action>
             </Card.Header>
             <Card.Content>
                 <p>RM24,500.00</p>
