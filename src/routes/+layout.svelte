377a43006af9ce109ecac57f284bf21d332f1903
ignatiusfairlight <lawaierang@gmail.com>
2025-07-23 15:43:11 +0800

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+	import '../app.css';
+	import * as NavigationMenu from '$lib/components/ui/navigation-menu/index.js';
+	import * as Drawer from '$lib/components/ui/drawer/index.js';
+	import { navigationMenuTriggerStyle } from '$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte';
+	import { onNavigate } from '$app/navigation';
+	import { MediaQuery } from 'svelte/reactivity';
+	import DrawerClose from '$lib/components/ui/drawer/drawer-close.svelte';
+
+	onNavigate((navigation) => {
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
+
+	const isDesktop = new MediaQuery('(min-width: 768px)');
+
+	let { children } = $props();
+</script>
+
+{#if isDesktop.current}
+	<div class="mt-5 mr-10 flex gap-45 items-center justify-center">
+		<!--Add fake bank logo here-->
+		<p>MyBank</p>
+		<NavigationMenu.Root>
+			<NavigationMenu.List class="flex gap-20">
+				<NavigationMenu.Item>
+					<NavigationMenu.Link>
+						{#snippet child()}
+							<a href="./" class={navigationMenuTriggerStyle()}>My Accounts</a>
+						{/snippet}
+					</NavigationMenu.Link>
+				</NavigationMenu.Item>
+				<NavigationMenu.Item>
+					<NavigationMenu.Link>
+						{#snippet child()}
+							<a href="cards" class={navigationMenuTriggerStyle()}>Cards</a>
+						{/snippet}
+					</NavigationMenu.Link>
+				</NavigationMenu.Item>
+				<NavigationMenu.Item>
+					<NavigationMenu.Link>
+						{#snippet child()}
+							<a href="loans" class={navigationMenuTriggerStyle()}>Loans/Financing</a>
+						{/snippet}
+					</NavigationMenu.Link>
+				</NavigationMenu.Item>
+				<NavigationMenu.Item>
+					<NavigationMenu.Link>
+						{#snippet child()}
+							<a href="wealth" class={navigationMenuTriggerStyle()}>Wealth</a>
+						{/snippet}
+					</NavigationMenu.Link>
+				</NavigationMenu.Item>
+			</NavigationMenu.List>
+		</NavigationMenu.Root>
+	</div>
+{:else}
+	<div class="ml-5 pt-5">
+		<Drawer.Root direction="top">
+			<!--Replace with three-line menu symbol-->
+			<Drawer.Trigger>Menu</Drawer.Trigger>
+			<Drawer.Content class="py-5 flex flex-col items-start justify-center gap-10">
+				<Drawer.Close>
+					<a href="./" class={navigationMenuTriggerStyle()}>My Accounts</a>
+				</Drawer.Close>
+				<DrawerClose>
+					<a href="cards" class={navigationMenuTriggerStyle()}>Cards</a>
+				</DrawerClose>
+				<Drawer.Close>
+					<a href="loans" class={navigationMenuTriggerStyle()}>Loan/Financing</a>
+				</Drawer.Close>
+				<Drawer.Close>
+					<a href="wealth" class={navigationMenuTriggerStyle()}>Wealth</a>
+				</Drawer.Close>
+			</Drawer.Content>
+		</Drawer.Root>
+	</div>
+{/if}
+
+<main class="p-16 min-h-[87dvh]">
+	{@render children()}
+</main>
+
+
+<footer class="flex flex-col items-center justify-center p-5">
+	<p>© 2025 MyBank Sdn Bhd. All Rights Reserved.</p>
+</footer>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+    import * as Card from "$lib/components/ui/card/index.js";
+</script>
+
+<div class="flex flex-col gap-10">
+    <h1 class="text-3xl">My Accounts</h1>
+    <div class="flex flex-col gap-5">
+        <Card.Root>
+            <Card.Header class="flex flex-col justify-center items-center">
+                <Card.Title>
+                    <h2 class="text-3xl md:text-4xl">RM 44,500.00</h2>
+                </Card.Title>
+                <Card.Description>
+                    <h3>Total Balance</h3>
+                </Card.Description>
+            </Card.Header>
+        </Card.Root>
+        <Card.Root class="bg-red-400">
+            <Card.Header class="flex flex-col justify-center items-start">
+                <Card.Title>
+                    <h2 class="text-2xl">Savings Account-Alpha</h2>
+                </Card.Title>
+                <Card.Description>
+                    <h3>7483 9162 5084</h3>
+                </Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <p>RM20,000.00</p>
+            </Card.Content>
+        </Card.Root>
+        <Card.Root class="bg-amber-400">
+            <Card.Header class="flex flex-col justify-center items-start">
+                <Card.Title>
+                    <h2 class="text-2xl">Current Account-i</h2>
+                </Card.Title>
+                <Card.Description>
+                    <h3>7483 9162 5084</h3>
+                </Card.Description>
+            </Card.Header>
+            <Card.Content>
+                <p>RM24,500.00</p>
+            </Card.Content>
+        </Card.Root>
+    </div>
+</div>
+
+
